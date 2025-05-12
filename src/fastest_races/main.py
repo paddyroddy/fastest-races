99408b7759ba331e8fafee9b39d340c3d44fa69f
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2025-05-12 17:07:39 +0100

    Get workable dataframe

--- src/fastest_races/main.py
+++ src/fastest_races/main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import bs4
 import urllib3
 
+
 def main(url: str) -> None:
     """_summary_.
 
@@ -10,12 +11,33 @@ def main(url: str) -> None:
     url
         _description_.
     """
-    response = urllib3.request("GET", url)
-    soup = BeautifulSoup(response.text, "lxml")
+    http = urllib3.PoolManager()
+    response = http.request("GET", url)
+    html_content = response.data.decode("utf-8")
+    soup = bs4.BeautifulSoup(html_content, "html.parser")
     table = soup.find("span", {"id": "cphBody_lblCachedRankingList"}).find("table")
-
+    df = pd.read_html(str(table), header=1, index_col=0)[0]
+    df.drop(
+        [
+            "Unnamed: 2",
+            "Unnamed: 3",
+            "PB",
+            "Unnamed: 5",
+            "Unnamed: 7",
+            "Year",
+            "Coach",
+            "Club",
+            "Unnamed: 13",
+        ],
+        axis=1,
+        inplace=True,
+    )
+    df["Date"] = pd.to_datetime(df["Date"])
+    df[["Venue", "County"]] = df["Venue"].str.split(",", expand=True)
+    df["County"] = df["County"].fillna("UK")
+    print()
 
 
 if __name__ == "__main__":
-    url = "https://www.thepowerof10.info/rankings/rankinglist.aspx?event=10K&agegroup=ALL&sex=M&year=2024"
+    url = "https://www.thepowerof10.info/rankings/rankinglist.aspx?event=HepI&agegroup=ALL&sex=M&year=2024"
     main(url)
